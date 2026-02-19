d1d5fa0f97d5311c045ae54e40f8d3ee1c20bd81
sateesh kumar <sateesh.202204033@tulas.edu.in>
2026-02-19 20:40:51 +0530

    updated files

--- schema/studentSchema.js
+++ schema/studentSchema.js
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose'
 
-const  studentSchema=mongoose.Schema({
-  name:String,
-  age:Number,
-  email:String,
+const studentSchema=mongoose.Schema({
+name:String,
+age:Number,
+email:String,
 })
 export default studentSchema;
